aaf14481987cf9f0a44455e9c8f1be3492c14122
ziMMi99 <simon@mvbmail.dk>
2024-01-10 12:01:17 +0100

    Added all fxml files for UI

--- src/main/java/com/example/kampregprogram/Application.java
+++ src/main/java/com/example/kampregprogram/Application.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
