0c06c8e4dfa003f26c2a1ab60cfe0b523a078714
AndersDue-git <150009507+AndersDue-git@users.noreply.github.com>
2024-01-10 12:05:01 +0100

    Team Class oprettet (Anders Due)

---
+++ src/main/java/com/example/kampregprogram/Team.java
@@ -0,0 +1,58 @@
+package com.example.kampregprogram;
+
+public class Team {
+    private int id;
+    private String name;
+    private int numberOfPlayers;
+    private int point;
+    private String teamCity
+
+    // Default Constructor
+    Public Team(){
+
+}
+// Set Constructor
+    public Team(int id, String name, int numberOfPlayers, int point, String teamCity) {
+        this.id = id;
+        this.name = name;
+        this.numberOfPlayers = numberOfPlayers;
+        this.point = point;
+        this.teamCity = teamCity;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", numberOfPlayers=" + numberOfPlayers +
+                ", point=" + point +
+                ", teamCity='" + teamCity + '\'' +
+                '}';
+    }
+
+
+// Getters
+    public int getId() {        return id;    }
+    public String getName() {        return name;    }
+    public int getNumberOfPlayers() {        return numberOfPlayers;    }
+    public String getTeamCity() {        return teamCity;    }
+
+// Setters
+       public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setNumberOfPlayers(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    public void setTeamCity(String teamCity) {
+        this.teamCity = teamCity;
+    }
+
+}
