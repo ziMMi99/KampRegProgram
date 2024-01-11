e932f262a56ad6955614ff6198d1d9a1eac24da5
ziMMi99 <simon@mvbmail.dk>
2024-01-11 11:29:09 +0100

    Changed Size of Panel to 800x600

--- src/main/java/com/example/kampregprogram/Application.java
+++ src/main/java/com/example/kampregprogram/Application.java
@@ -14,7 +14,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(),800,600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
