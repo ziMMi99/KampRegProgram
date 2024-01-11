c66aa4966b4efadffa92b59e92c03d7bd08c7e08
ziMMi99 <simon@mvbmail.dk>
2024-01-11 11:26:42 +0100

    ToolBar added

--- src/main/java/com/example/kampregprogram/Controller.java
+++ src/main/java/com/example/kampregprogram/Controller.java
@@ -1,14 +1,32 @@
 package com.example.kampregprogram;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToKampRapport(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("KampRapport.fxml"));
+        System.out.println("heh");
+        Parent root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
