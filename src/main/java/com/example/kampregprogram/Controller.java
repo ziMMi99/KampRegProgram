c8f5969a6e109bef691634c283f93d2109a28530
zitmaster <Victorcebula@yahoo.dk>
2024-01-12 10:10:08 +0100

    Deleted some import that was not in use

--- src/main/java/com/example/kampregprogram/Controller.java
+++ src/main/java/com/example/kampregprogram/Controller.java
@@ -1,20 +1,13 @@
 package com.example.kampregprogram;
 
 import com.example.kampregprogram.data.DataLayer;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import org.w3c.dom.Node;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
